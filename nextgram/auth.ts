59c89906719dac9af9c383a3364c0d35154d771e
Spinelli666 <bernardospinelli@hotmail.com.br>
2025-02-24 09:53:18 -0300

    (feat): Ajustando a Autenticação do Projeto

--- nextgram/auth.ts
+++ nextgram/auth.ts
@@ -13,17 +13,27 @@ const config = {
     session: {strategy: 'jwt'},
     providers: [google],
     callbacks: {
-        authorized({request, auth}) {
-
-            const {pathname} = request.nextUrl
-
-            if(pathname === '/middleware') {
-                return !!auth
-            }
-
-            return true
-        }
-    }
+        session({ session, token }) {
+            if(token.sub) session.user.userId = token.sub;
+            return session;
+        },
+    },
+    pages: {
+        signIn: "/signIn",
+    },
 } satisfies NextAuthConfig;
 
 export const { handlers, auth, signIn, signOut } = NextAuth(config);
+
+// Providers para utilizar na minha página, dá para fazer o mapeamento
+interface ProviderWithId {
+    id: string;
+    name: string;
+}
+
+// Mapear os providers
+// Com isso, agora podemos acessar os providers na pag. de login
+export const providerMap = config.providers.map((provider) => {
+    const typedProvider = provider as unknown as ProviderWithId
+    return {id: typedProvider.id, name: typedProvider.name}
+})
