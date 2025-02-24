93620face449e9d633f0df07f0fe89ed72150dc7
Spinelli666 <bernardospinelli@hotmail.com.br>
2025-02-24 11:25:58 -0300

    (feat): Concluido Navbar

---
+++ nextgram/src/components/Button.tsx
@@ -0,0 +1,19 @@
+import { ComponentProps } from "react"
+
+type ButtonProps = ComponentProps<"button"> & {
+    text: string;
+    danger?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({text, danger = false, ...props}) => {
+  return (
+    <button 
+        data-danger={danger} 
+        className="h-8 text-sm font-medium text-white py-1 px-6 rounded flex items-center bg-blue-800 houver:bg-blue-700 data-[danger=true]:bg-red-500 data-[danger=true]:hover:bg-red-400"
+        >
+          {text}
+        </button>
+  )
+}
+
+export default Button
--- nextgram/src/components/Navbar.tsx
+++ nextgram/src/components/Navbar.tsx
@@ -1,6 +1,9 @@
 import { getUserByEmail } from "@/actions"
 import { auth, signIn, signOut } from "auth"
 import Link from "next/link"
+import Image from "next/image"
+import Button from "./Button"
+import ButtonLink from "./ButtonLink"
 
 const Navbar = async () => {
 
@@ -15,16 +18,43 @@ const Navbar = async () => {
         {user ? (
           // logado
           <div className="flex gap-4 items-center">
-            <p>{user.name}</p>
+            <p className="text-white font-medium">{user.name}</p>
+            {user.image && (
+              <Image 
+                src={user.image}
+                alt={`Perfil de: ${user.name}`}
+                className='w-10 h-10 rounded-full'
+                width={40}
+                height={40}
+              />
+            )}
+            <Link 
+              href={'/profile'}
+              className="text-white font-medium hover:text-zinc-200"
+              >
+                Perfil
+            </Link>
+            <Link 
+              href={'/post/new'}
+              className="text-white font-medium hover:text-zinc-200"
+              >
+                Criar postegame
+            </Link>
+            <Link 
+              href={'/my-posts'}
+              className="text-white font-medium hover:text-zinc-200"
+              >
+                Minhas postagens
+            </Link>
             <form action={async () => {
                 'use server'
                 await signOut()
             }}>
-              <button className="bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded">Sair</button>
+              <Button text="Sair" danger={true} type='submit' />
             </form>
           </div>
         ) : (
-          <Link href="/signin" className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded">Entrar</Link>
+          <ButtonLink text='Entrar' url='/signin' />
 
         )}
       </div>
