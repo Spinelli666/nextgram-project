e2977d65f9fbc8142f717008d522ee83fb683e25
Spinelli666 <bernardospinelli@hotmail.com.br>
2025-02-24 10:02:09 -0300

    (feat): Criando as Páginas do Projeto

---
+++ nextgram/src/app/post/new/page.tsx
@@ -0,0 +1,9 @@
+const NewPostPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default NewPostPage
--- nextgram/src/components/Navbar.tsx
+++ nextgram/src/components/Navbar.tsx
@@ -10,25 +10,20 @@ const Navbar = async () => {
       <Link href='/' className="text-white text-lg font-bold">Home</Link>
       <div>
         {session && session.user ? (
-            // logado
-            <div className="flex gap-4 items-center">
-                {/* login social => name, email, image */}
-                <p>{session.user.name}</p>
-                <form action={async () => {
-                  'use server'
-                  await signOut()
-                }}>
-                  <button className="bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded">Sair</button>
-                </form>
-            </div>
-        ) : (
-            // delogado
+          // logado
+          <div className="flex gap-4 items-center">
+            {/* login social => name, email, image */}
+            <p>{session.user.name}</p>
             <form action={async () => {
                 'use server'
-                await signIn()
+                await signOut()
             }}>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded">Entrar</button>
+              <button className="bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded">Sair</button>
             </form>
+          </div>
+        ) : (
+          <Link href="/signin" className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded">Entrar</Link>
+
         )}
       </div>
     </div>
